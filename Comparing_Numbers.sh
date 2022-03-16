31987cd5b248a84e7bddd478bc7a742f51192f46
melekalan <dt.bmaslan@gmail.com>
2022-03-16 09:52:03 +0300

    linux sh added

--- Comparing_Numbers.sh
+++ Comparing_Numbers.sh
@@ -7,12 +7,7 @@ Exactly one of the following lines:
 - X is equal to Y
 
 Input Format
-
 Two lines containing one integer each ( and , respectively).
-
-Constraints
-
--
 
 Output Format
 
@@ -24,27 +19,23 @@ Exactly one of the following lines:
 Sample Input
 
 Sample Input 1
-
 5  
 2  
 Sample Input 2
-
 2
 2  
 Sample Input 3
-
 2
 3  
 Sample Output
 
 Sample Output 1
-
 X is greater than Y  
+
 Sample Output 2
+X is equal to Y  
 
-X is equal to Y   
 Sample Output 3
-
 X is less than Y 
 COMMENT
 
