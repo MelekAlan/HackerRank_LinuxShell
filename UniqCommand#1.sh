2b8569d84b5cd6dd422405bebf7982e99aa72e60
melekalan <dt.bmaslan@gmail.com>
2022-04-03 21:15:11 +0300

    uniq1 added

---
+++ UniqCommand#1.sh
@@ -0,0 +1,8 @@
+<<'COMMENT'
+In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+Given a text file, remove the consecutive repetitions of any line.
+COMMENT
+
+#!/bin/bash
+
+uniq 
